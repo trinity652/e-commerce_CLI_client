90a658da8a9813e8cfcde416de059d2f90f0558c
trinity652 <abhilashajha96@gmail.com>
2020-09-17 17:32:19 +0530

    Added main_script

--- main_script.py
+++ main_script.py
@@ -2,4 +2,12 @@ import user
 import admin
 import mysql.connector
 
+print("Welcome to ECart!")
+print("If you are an Admin press 1")
+print("If you are a Customer press 2")
+Option=int(input())
+if Option == 1:
+    
+
+elif Option ==2:
 
